00511e3233cb7ddb605cfb5a3c44d6dad49e83dd
nagisa599 <nagi46460510@icloud.com>
2024-09-01 10:03:18 +0900

    test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   out: './db/migrations',
   dbCredentials: {
     url: process.env.POSTGRES_URL || '',
+     ssl: {
+      rejectUnauthorized: false, // 開発環境でのみ使用し、本番環境では信頼できる証明書を使用
+      // 以下の行を追加して、証明書ファイルを指定することもできます（必要な場合）
+      // ca: fs.readFileSync('/path/to/your/certificate.crt').toString(),
+    }
   },
 });
