eb0014eff8d9075031dec6014ce5f3a6bb6751db
nagisa599 <nagi46460510@icloud.com>
2024-09-01 09:52:57 +0900

    drizzleの変更

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema.ts',
+  breakpoints: true,
   out: './db/migrations',
   dbCredentials: {
     url: process.env.POSTGRES_URL || '',
